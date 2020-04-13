a785a95a802397fdeffb2eae251f220a913ebd7e
dr-matt-smith <matt.smith@itb.ie>
2020-04-13 18:32:43 +0100

    added files to project

---
+++ Assets/LoadFromAssetBundle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using System.IO;
+
+public class LoadFromAssetBundle : MonoBehaviour
+{
+    public String assetBundleName = "cubered";
+    public String prefabName = "CubeRed";
+    
+    void Start()
+    {
+        var myLoadedAssetBundle 
+            = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, assetBundleName));
+        if (myLoadedAssetBundle == null) {
+            Debug.Log("Failed to load AssetBundle!");
+            return;
+        }
+        
+        // create and instantiate prefab - red Cube
+        var prefab = myLoadedAssetBundle.LoadAsset<GameObject>(prefabName);
+        Instantiate(prefab);
+    }
+}
